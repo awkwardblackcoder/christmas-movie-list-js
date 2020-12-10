9c68afb3b7de816efcfdef68ba06e9b856d2a6c4
PHOEBE MICHEL <poebe.piezema-estime@t-mobile.com>
2020-12-09 22:43:00 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+var movie1 = document.getElementById('movie1');
+movie1.textContent = "Best Man Holiday";
+
+var movie2 = document.getElementById('movie2');
+movie2.textContent = "Home Alone";
+
+var movie3 = document.getElementById('movie3');
+movie3.textContent = "Bad Santa";
+
+var movie4 = document.getElementById('movie4');
+movie4.textContent = "The Preacher's Wife";
+
+var movie5 = document.getElementById('movie5');
+movie5.textContent = "Almost Christmas";
+
+var movie6 = document.getElementById('movie6');
+movie6.textContent = "Jingle Jangle: A Christmas Story";
+
+var movie7 = document.getElementById('movie7');
+movie7.textContent = "This Christmas";
+
+var movie8 = document.getElementById('movie8');
+movie8.textContent = "Bad Moms Christmas";
+
+var movie9 = document.getElementById('movie9');
+movie9.textContent = "The Night Before";
+
+var movie10 = document.getElementById('movie10');
+movie10.textContent = "Daddy's Home 2";
+
+var movie11 = document.getElementById('movie11');
+movie11.textContent = "Office Christmas Party";
+
+var movie12 = document.getElementById('movie12');
+movie12.textContent = "Love Actually";
+
+var movie13 = document.getElementById('movie13');
+movie13.textContent = "Black Nativity";
+
+var movie14 = document.getElementById('movie14');
+movie14.textContent = "Love the Coopers";
+
+var movie15 = document.getElementById('movie15');
+movie15.textContent = "The Family Stone";
+
+var movie16 = document.getElementById('movie16');
+movie16.textContent = "A Very Harold & Kumar Christmas";
+
+var movie17 = document.getElementById('movie17');
+movie17.textContent = "While You Were Sleeping";
+
+var movie18 = document.getElementById('movie18');
+movie18.textContent = "Elf";
+
+var movie19 = document.getElementById('movie19');
+movie19.textContent = "Die Hard";
+
+var movie20 = document.getElementById('movie20');
+movie20.textContent = "Home Alone";
+
+
+
+
