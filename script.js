0f2eeac8c0a820e24cc6d3c65495f8d37edf087f
PHOEBE MICHEL <poebe.piezema-estime@t-mobile.com>
2020-12-22 08:21:56 -0500

    add movie names to array

--- script.js
+++ script.js
@@ -1,63 +1,86 @@
-var movie1 = document.getElementById('movie1');
-movie1.textContent = "Best Man Holiday";
+var movies = [
+  "Best Man Holiday",
+  "Home Alone",
+  "Bad Santa",
+  "The Preacher's Wife",
+  "Almost Christmas",
+  "Jingle Jangle: A Christmas Story",
+  "This Christmas",
+  "The Night Before",
+  "Bad Moms Christmas",
+  "Daddy's Home 2",
+  "Office Christmas Party",
+  "Love Actually",
+  "Black Nativity",
+  "Love the Coopers",
+  "The Family Stone",
+  "A Very Harold & Kumar Christmas",
+  "While You Were Sleeping",
+  "Elf",
+  "Die Hard",
+  "Home Alone",
+];
 
-var movie2 = document.getElementById('movie2');
-movie2.textContent = "Home Alone";
+var movie1 = document.getElementById("movie1");
+movie1.textContent = movies[0];
 
-var movie3 = document.getElementById('movie3');
-movie3.textContent = "Bad Santa";
+var movie2 = document.getElementById("movie2");
+movie2.textContent = movies[1];
 
-var movie4 = document.getElementById('movie4');
-movie4.textContent = "The Preacher's Wife";
+var movie3 = document.getElementById("movie3");
+movie3.textContent = movies[2];
 
-var movie5 = document.getElementById('movie5');
-movie5.textContent = "Almost Christmas";
+var movie4 = document.getElementById("movie4");
+movie4.textContent = movies[3];
 
-var movie6 = document.getElementById('movie6');
-movie6.textContent = "Jingle Jangle: A Christmas Story";
+var movie5 = document.getElementById("movie5");
+movie5.textContent = movies[4];
 
-var movie7 = document.getElementById('movie7');
-movie7.textContent = "This Christmas";
+var movie6 = document.getElementById("movie6");
+movie6.textContent = movies[5];
 
-var movie8 = document.getElementById('movie8');
-movie8.textContent = "Bad Moms Christmas";
+var movie7 = document.getElementById("movie7");
+movie7.textContent = movies[6];
 
-var movie9 = document.getElementById('movie9');
-movie9.textContent = "The Night Before";
+var movie8 = document.getElementById("movie8");
+movie8.textContent = movies[7];
 
-var movie10 = document.getElementById('movie10');
-movie10.textContent = "Daddy's Home 2";
+var movie9 = document.getElementById("movie9");
+movie9.textContent = movies[8];
 
-var movie11 = document.getElementById('movie11');
-movie11.textContent = "Office Christmas Party";
+var movie10 = document.getElementById("movie10");
+movie10.textContent = movies[9];
 
-var movie12 = document.getElementById('movie12');
-movie12.textContent = "Love Actually";
+var movie11 = document.getElementById("movie11");
+movie11.textContent = movies[10];
 
-var movie13 = document.getElementById('movie13');
-movie13.textContent = "Black Nativity";
+var movie12 = document.getElementById("movie12");
+movie12.textContent = movies[11];
 
-var movie14 = document.getElementById('movie14');
-movie14.textContent = "Love the Coopers";
+var movie13 = document.getElementById("movie13");
+movie13.textContent = movies[12];
 
-var movie15 = document.getElementById('movie15');
-movie15.textContent = "The Family Stone";
+var movie14 = document.getElementById("movie14");
+movie14.textContent = movies[13];
 
-var movie16 = document.getElementById('movie16');
-movie16.textContent = "A Very Harold & Kumar Christmas";
+var movie15 = document.getElementById("movie15");
+movie15.textContent = movies[14];
 
-var movie17 = document.getElementById('movie17');
-movie17.textContent = "While You Were Sleeping";
+var movie16 = document.getElementById("movie16");
+movie16.textContent = movies[15];
 
-var movie18 = document.getElementById('movie18');
-movie18.textContent = "Elf";
+var movie17 = document.getElementById("movie17");
+movie17.textContent = movies[16];
 
-var movie19 = document.getElementById('movie19');
-movie19.textContent = "Die Hard";
+var movie18 = document.getElementById("movie18");
+movie18.textContent = movies[17];
 
-var movie20 = document.getElementById('movie20');
-movie20.textContent = "Home Alone";
+var movie19 = document.getElementById("movie19");
+movie19.textContent = movies[18];
 
+var movie20 = document.getElementById("movie20");
+movie20.textContent = movies[19];
 
-
+  var movies = document.querySelectorAll('li input'); 
+  
 
