a967b86158b0bfe79587a8bd59e5376ee3c984ee
PHOEBE MICHEL <poebe.piezema-estime@t-mobile.com>
2021-01-21 00:08:00 -0500

    loop through array and display number of movies watched on page based on checked items.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # christmas-movie-list-js
 
-Spreading a little holiday cheer "tech style" with this simple Christmas Movie List, built entirely with HTML, CSS & Vanilla JavaScript. 
+Spreading a little holiday cheer "tech style" with this simple Christmas Movie List, built entirely with HTML, CSS & Vanilla JavaScript.
+
+Concepts covered in this project:
+
+- arrays: script stores names of movies in an array.
+- for loops: script loops through array to add movie name to each label element in document
+- forEach: script loops through each check box to verify if it is checked and calculates number of checkboxes checked. Then it displays that number on the webpage.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var movies = [
+var movieNames = [
   "Best Man Holiday",
   "Home Alone",
   "Bad Santa",
@@ -21,66 +21,24 @@ var movies = [
   "Home Alone",
 ];
 
-var movie1 = document.getElementById("movie1");
-movie1.textContent = movies[0];
+var movieCheckList = document.querySelectorAll("label");
+var numMoviesWatched = document.getElementById("watched");
 
-var movie2 = document.getElementById("movie2");
-movie2.textContent = movies[1];
+for (i = 0; i < movieCheckList.length; i++) {
+  movieCheckList[i].textContent = movieNames[i];
+}
 
-var movie3 = document.getElementById("movie3");
-movie3.textContent = movies[2];
+var numofMovies = 0;
+var movieCheckboxes = document.querySelectorAll("input");
+numMoviesWatched.textContent = numofMovies;
 
-var movie4 = document.getElementById("movie4");
-movie4.textContent = movies[3];
-
-var movie5 = document.getElementById("movie5");
-movie5.textContent = movies[4];
-
-var movie6 = document.getElementById("movie6");
-movie6.textContent = movies[5];
-
-var movie7 = document.getElementById("movie7");
-movie7.textContent = movies[6];
-
-var movie8 = document.getElementById("movie8");
-movie8.textContent = movies[7];
-
-var movie9 = document.getElementById("movie9");
-movie9.textContent = movies[8];
-
-var movie10 = document.getElementById("movie10");
-movie10.textContent = movies[9];
-
-var movie11 = document.getElementById("movie11");
-movie11.textContent = movies[10];
-
-var movie12 = document.getElementById("movie12");
-movie12.textContent = movies[11];
-
-var movie13 = document.getElementById("movie13");
-movie13.textContent = movies[12];
-
-var movie14 = document.getElementById("movie14");
-movie14.textContent = movies[13];
-
-var movie15 = document.getElementById("movie15");
-movie15.textContent = movies[14];
-
-var movie16 = document.getElementById("movie16");
-movie16.textContent = movies[15];
-
-var movie17 = document.getElementById("movie17");
-movie17.textContent = movies[16];
-
-var movie18 = document.getElementById("movie18");
-movie18.textContent = movies[17];
-
-var movie19 = document.getElementById("movie19");
-movie19.textContent = movies[18];
-
-var movie20 = document.getElementById("movie20");
-movie20.textContent = movies[19];
-
-  var movies = document.querySelectorAll('li input'); 
-  
-
+movieCheckboxes.forEach(function (checkbox) {
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      numofMovies++;
+    } else {
+      numofMovies--;
+    }
+    numMoviesWatched.textContent = numofMovies;
+  });
+});
